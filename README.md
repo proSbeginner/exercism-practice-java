e62c37fcc151d29047506df0564a642baabb23f0
kittipong.p <kittipnog.p@arise.tech>
2025-05-22 17:53:43 +0700

    add task2, Binary

--- README.md
+++ README.md
@@ -15,3 +15,35 @@ AccumulateTest > accumulateUpperCases PASSED
 
 AccumulateTest > accumulateSquares PASSED
 ```
+### 2. binary
+```
+BinaryTest > testBinary11IsDecimal3 PASSED
+
+BinaryTest > testNumberAndWordWhitespaceSeparatedInvalid PASSED
+
+BinaryTest > testBinaryIgnoresLeadingZeros PASSED
+
+BinaryTest > testNumberWithInternalNonBinaryCharactersInvalid PASSED
+
+BinaryTest > testBinary1IsDecimal1 PASSED
+
+BinaryTest > testNumberWithTrailingNonBinaryCharactersInvalid PASSED
+
+BinaryTest > testBinary1001IsDecimal9 PASSED
+
+BinaryTest > testBinary10IsDecimal2 PASSED
+
+BinaryTest > testNumberWithLeadingNonBinaryCharactersInvalid PASSED
+
+BinaryTest > testNumberContainingNonBinaryDigitInvalid PASSED
+
+BinaryTest > testBinary0IsDecimal0 PASSED
+
+BinaryTest > test2NotValidBinaryDigit PASSED
+
+BinaryTest > testBinary100IsDecimal4 PASSED
+
+BinaryTest > testBinary11010IsDecimal26 PASSED
+
+BinaryTest > testBinary10001101000IsDecimal1128 PASSED
+```
--- binary/src/main/java/Binary.java
+++ binary/src/main/java/Binary.java
@@ -1,10 +1,28 @@
 public class Binary {
-
+    private String input;
     public Binary(String input) {
-        throw new UnsupportedOperationException("Please implement the Binary constructor.");
+        this.input = input;
     }
 
     public int getDecimal() {
-        throw new UnsupportedOperationException("Please implement the Binary.getDecimal() method.");
+        // if(input.equals("1")) return 1;
+        // if(input.equals("10")) return 2;
+        // if(input.equals("11")) return 3;
+        // if(input.equals("100")) return 4;
+        // if(input.equals("1001")) return 9;
+        // if(input.equals("11010")) return 26;
+        // if(input.equals("10001101000")) return 1128;
+        // if(input.equals("000011111")) return 31;
+        // if(input.equals("2")) return 0;
+        // if(input.equals("01201")) return 0;
+        // if(input.equals("10nope")) return 0;
+        // if(input.equals("nope10")) return 0;
+        // if(input.equals("10nope10")) return 0;
+        // if(input.equals("001 nope")) return 0;
+        // return 0;
+        
+        if(input.chars().filter(c -> !(c == '0' || c == '1')).count() > 0) return 0;
+        if(input.chars().anyMatch(Character::isLetter)) return 0;
+        return Integer.parseInt(input, 2);
     }
 }
