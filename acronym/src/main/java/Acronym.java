c00a2b1abc675b4392f1d14ce0f72a3273f16430
kittipong.p <kittipnog.p@arise.tech>
2025-05-27 22:16:20 +0700

    add task4, Acronym

--- acronym/src/main/java/Acronym.java
+++ acronym/src/main/java/Acronym.java
@@ -1,11 +1,23 @@
 class Acronym {
+    private String phrase;
 
     Acronym(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.phrase = phrase;
     }
 
     String get() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] words = phrase.replaceAll(",", "").split("\\s+|-|_");
+        String r = "";
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                r += word.charAt(0);
+            }
+        }
+        return r.toUpperCase();
     }
 
+    public static void main(String[] args) {
+        String r = new Acronym("Complementary metal-oxide semiconductor").get();
+        System.out.println(r);
+    }
 }
